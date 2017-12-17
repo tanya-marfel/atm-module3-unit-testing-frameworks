f582f5a303a196e1fa7db2781f2ed2493c54ab73
Tatsiana Marfel <tatsiana_marfel@epam.com>
2017-12-17 12:24:17 +0300

    Changes JUnit tests location

--- JUnitCalculatorTests/src/test/java/com/epam/junit/basictests/BasicCosTest.java
+++ JUnitCalculatorTests/src/test/java/com/epam/junit/basictests/BasicCosTest.java
@@ -1,8 +1,8 @@
 package com.epam.junit.basictests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCosTest extends ParentCalculatorTests {
 
